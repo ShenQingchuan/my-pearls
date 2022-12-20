e198b41c726d714422ab3e9b6176d409e19fd79c
ShenQingchuan <tmy1106@outlook.com>
2022-12-20 15:54:14 +0800

    chore: fix lint for workflow.

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,5 +2,6 @@ const { defineConfig } = require('eslint-define-config')
 
 module.exports = defineConfig({
   root: true,
+  ignorePatterns: ['node_modules', 'dist', 'docs'],
   extends: ['@sxzz/eslint-config-ts', '@sxzz/eslint-config-prettier'],
 })
