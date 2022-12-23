c2d30386d9649a8d6b2a9c2521ea27a3b4b0b5df
ShenQingchuan <tmy1106@outlook.com>
2022-12-23 21:51:48 +0800

    chore: release v0.0.7

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,11 +1,10 @@
 import { defineConfig } from 'tsup'
 
 export default defineConfig({
-  entry: ['./src/**'],
+  entry: ['./src/index.ts'],
   format: ['cjs', 'esm'],
   target: 'node16',
   clean: true,
   dts: true,
-  sourcemap: true,
   splitting: false,
 })
