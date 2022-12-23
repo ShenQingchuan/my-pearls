295ebf411a355b836e052602688c8269b1ad4722
ShenQingchuan <tmy1106@outlook.com>
2022-12-23 21:41:45 +0800

    chore: align with node.js standard module resolve strategy [ci skip]

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,11 +1,10 @@
 import { defineConfig } from 'tsup'
 
 export default defineConfig({
-  entry: ['./src/**'],
-  format: ['cjs', 'esm'],
+  entry: ['./src/index.ts'],
+  format: ['esm', 'cjs'],
   target: 'node16',
   clean: true,
   dts: true,
   splitting: false,
-  legacyOutput: true,
 })
